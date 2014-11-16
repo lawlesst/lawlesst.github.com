6a7c7ade1cbcc9a59246461f3594e68563bfd1c4
Ted Lawless <lawlesst@gmail.com>
2014-11-16 12:53:52 -0500

    Update.

--- publish.sh
+++ publish.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
-
+#build html
 pelican content -o output -s settings.py
+#Move output to ghp branch
 ghp-import output
+#Push output
 git push origin gh-pages:master
+#Push source too
+git push origin pelican:pelican
