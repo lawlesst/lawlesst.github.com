c02a6baa00c288eb94acd06ba322b699061501ae
Ted Lawless <lawlesst@gmail.com>
2014-11-16 14:26:39 -0500

    Change publish commit message.

--- publish.sh
+++ publish.sh
@@ -3,7 +3,7 @@ set -e
 #build html
 pelican content -o output -s settings.py
 #Move output to ghp branch
-ghp-import output
+ghp-import output -m Update site
 #Push output
 git push origin gh-pages:master
 #Push source too
