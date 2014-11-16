6f7d667c51a94b0c574cb329ad0ab4636db76852
Ted Lawless <lawlesst@gmail.com>
2014-11-16 14:27:29 -0500

    Fix order.

--- publish.sh
+++ publish.sh
@@ -3,7 +3,7 @@ set -e
 #build html
 pelican content -o output -s settings.py
 #Move output to ghp branch
-ghp-import output -m Update site
+ghp-import -m "Update site" output
 #Push output
 git push origin gh-pages:master
 #Push source too
