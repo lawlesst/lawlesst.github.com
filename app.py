ca1e0fbf336c23eb9fa6846483c96c487fd3c7ff
Ted Lawless <lawlesst@gmail.com>
2023-08-18 16:24:20 -0400

    Update index site and template.

--- app.py
+++ app.py
@@ -1,21 +1,16 @@
-
 """
 Flask Frozen app to generate HTML for github pages.
 """
 
-from datetime import datetime
 import glob
 import os
+from datetime import datetime
 from html.parser import HTMLParser
 
-from flask import Flask
-from flask import render_template
-from flask import url_for
-from flask import make_response
-
-from bs4 import BeautifulSoup
 import dateparser
 import markdown2
+from bs4 import BeautifulSoup
+from flask import Flask, make_response, render_template, url_for
 
 app = Flask(__name__)
 
@@ -28,6 +23,7 @@ link = "http://lawlesst.github.io/"
 # see: https://stackoverflow.com/a/55825140/758157
 class HTMLFilter(HTMLParser):
     text = ""
+
     def handle_data(self, data):
         self.text += data
 
@@ -45,7 +41,7 @@ def read_post(path):
         "slug": slug,
         "date": date,
         "date_obj": dateparser.parse(date),
-        "html": html
+        "html": html,
     }
 
 
@@ -59,36 +55,36 @@ def index_posts():
     return sorted_posts
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    posts = index_posts()[:10]
-    return render_template('index.html', recent_posts=posts)
+    posts = index_posts()[:5]
+    return render_template("index.html", recent_posts=posts)
 
 
-@app.route('/archive')
-@app.route('/archives.html')
+@app.route("/archive")
+@app.route("/archives.html")
 def archive():
     posts = index_posts()
-    return render_template('archive.html', recent_posts=posts)
+    return render_template("archive.html", recent_posts=posts)
 
 
-@app.route('/notebook/<slug>.html')
+@app.route("/notebook/<slug>.html")
 def post(slug):
     post = read_post(os.path.join(posts_dir, f"{slug}.md"))
-    return render_template('post.html', title=post["title"], date=post["date"], text=post["html"])
+    return render_template("post.html", title=post["title"], date=post["date"], text=post["html"])
 
 
-@app.route('/<slug>.html')
+@app.route("/<slug>.html")
 def page(slug):
     with open(f"{pages_dir}/{slug}.md") as inf:
         title = next(inf).split(":")[1]
         slug = next(inf).split(":")[1]
         md = inf.read()
     html = markdown2.markdown(md)
-    return render_template('page.html', title=title, text=html)
+    return render_template("page.html", title=title, text=html)
 
 
-@app.route('/feed.rss')
+@app.route("/feed.rss")
 def rss():
     posts = index_posts()[:10]
     rss_posts = []
@@ -107,9 +103,9 @@ def rss():
         "description": "Work notebook",
         "feed_url": f"{link}/{url_for('rss')}",
         "pub_date": datetime.now(),
-        "posts": rss_posts
+        "posts": rss_posts,
     }
-    template = render_template('rss.xml.jinja2', **d)
+    template = render_template("rss.xml.jinja2", **d)
     response = make_response(template)
-    response.headers['Content-Type'] = 'application/xml'
+    response.headers["Content-Type"] = "application/xml"
     return response
